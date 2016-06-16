efd50946c9384f536be998ca3050812fc9f6b429
Jansen <jansenli89@gmail.com>
2016-06-16 17:35:14 -0400

    socketworking

--- server/io/index.js
+++ server/io/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var socketio = require('socket.io');
 var io = null;
+var data = {}
 
 module.exports = function(server) {
 
@@ -10,17 +11,34 @@ module.exports = function(server) {
 
     // console.log(io)
 
+    var onlineplayers = []
     io.on('connection', function(socket) {
-        // Now have access to socket, wowzers!
-        console.log('connected: ', socket.id)
-        socket.emit('anything', { data: 'ok' })
 
-        socket.on('newPlayer', function(data) {
-          socket.emit('newPlayer', data)
+        // Now have access to socket, wowzers!
+        console.log('A new client has connected!');
+        console.log(socket.id);
+
+        var roomName;
+        socket.on('joinRoom', function (room) {
+          roomName = room;
+          socket.join(roomName);
+          // if (!data[roomName]) {
+          //   data[roomName] = [];
+          // } else {
+          //   socket.emit('board', data[roomName]);
+          // }
+          console.log('here')
+          socket.broadcast.to(roomName).emit('justTesting', 'something')
+        });
+
+        socket.on('completeMove', function(){
+          socket.broadcast.to(roomName).emit('newBoardData', data[roomName])
         })
 
         socket.on('disconnect', function() {
-            console.log('Client disconnected.');
+            console.log('A client has disconnected :(');
+            console.log(socket.id);
+            socket.broadcast.to(roomName).emit('playerDC', 'some data about player')
         });
 
     });
