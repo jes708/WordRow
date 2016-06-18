0c384d7137c8463800dab4acc9bce386f0c95be2
Jansen <jansenli89@gmail.com>
2016-06-18 18:51:40 -0400

    i love bugs

--- browser/js/grid/rooms.states.js
+++ browser/js/grid/rooms.states.js
@@ -1,6 +1,18 @@
 app.config(function($stateProvider) {
-  $stateProvider.state('rooms', {
-    url: '/rooms/:roomName',
-    templateUrl: 'js/grid/room.template.html'
-  })
+    $stateProvider.state('rooms', {
+        cache: false,
+        url: '/rooms/:roomName',
+        templateUrl: 'js/grid/room.template.html',
+        controller: 'WordGameController',
+        resolve: {
+            location: function() {
+                let location = window.location.pathname
+                if (location === '/') {
+                    return 'root'
+                } else {
+                    return location.match(/\/rooms\/(\d*\w*)\/?/i)[1]
+                }
+            }
+        }
+    })
 })
