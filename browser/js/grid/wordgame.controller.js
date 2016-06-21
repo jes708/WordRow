04250afa0e3dfee81067905bd93694c6282d2f1c
Jansen <jansenli89@gmail.com>
2016-06-21 11:33:50 -0400

    socketfix

--- browser/js/grid/wordgame.controller.js
+++ browser/js/grid/wordgame.controller.js
@@ -1,4 +1,7 @@
 app.controller("WordGameController", function(GridGameFactory, $state, $stateParams, UserFactory, $scope, Socket, GameFactory, roomFactory, WordFactory) {
+    console.log('wordgamecontroller ran')
+    Socket.disconnect();
+    Socket.connect();
     GridGameFactory.ScopeDecorator($scope);
 
     UserFactory.getId()
@@ -33,6 +36,7 @@ app.controller("WordGameController", function(GridGameFactory, $state, $statePar
     $scope.createPot = WordFactory.createPot
 
     Socket.on('connect', function() {
+        console.log('socket connect ran')
         let location = function() {
             let location = window.location.pathname
             if (location === '/') {
@@ -43,6 +47,7 @@ app.controller("WordGameController", function(GridGameFactory, $state, $statePar
         }
 
         $scope.roomName = location()
+        console.log($scope.roomName)
 
         roomFactory.getRoom($scope.roomName)
             .then(function(roomInfo) {
